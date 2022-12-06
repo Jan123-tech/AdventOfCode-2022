6a684006737bb151ffe6f900d495f4c32162cb7e
Ian <ian.warburton@lbresearch.com>
2022-12-06 18:37:34 +0000

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,20 @@
+﻿var index = 0;
+var bSize = 14;
+var b = new Queue<char>();
+
+foreach (var c in File.ReadAllText("data.txt"))
+{
+	index++;
+	b.Enqueue(c) ;
+
+	if (b.Count() < bSize)
+		continue;
+
+	if (b.Count() > bSize)
+		b.Dequeue();
+
+	if (b.GroupBy(x => x).Count() == bSize)
+		break;
+}
+
+Console.WriteLine(index);
