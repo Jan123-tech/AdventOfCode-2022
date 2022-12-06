191c107106cedd84af2d877f28c880d3d3af0dd6
Ian <ian.warburton@lbresearch.com>
2022-12-06 20:07:36 +0000

    Calculate result

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,32 +1,40 @@
-﻿var wins = new HashSet<(Play, Play)>
+﻿Play ToPlay(char s) => s == 'A' ? Play.Rock : s == 'B' ? Play.Paper : Play.Scissors;
+Result ToResult(char s) => s == 'X' ? Result.Loss : s == 'Y' ? Result.Draw : Result.Win;
+
+Play PlayForResult(Play play, Result result) =>
+    result == Result.Draw ? play : GetDecisivePlay(play, result);
+
+Play GetDecisivePlay(Play play, Result result)
 {
-    { (Play.Paper, Play.Rock) },
-    { (Play.Scissors, Play.Paper) },
-    { (Play.Rock, Play.Scissors) },
-};
+    if (result == Result.Draw)
+        throw new ArgumentException();
 
-Play targetToPlay((Play, Target) pair) =>
-    pair.Item2 == Target.Draw ?
-        pair.Item1 :
-        pair.Item2 == Target.Win ?
-            wins!.First(x => x.Item2 == pair.Item1).Item1 :
-            wins!.First(x => x.Item1 == pair.Item1).Item2;
+    var playInt = (int) play;
+    var inc = result == Result.Win ? 1 : 2;
+    var newPlay = playInt + inc;
+    var norm = newPlay % 3;
 
-Play toPlay(string s) => s == "A" ? Play.Rock : s == "B" ? Play.Paper : Play.Scissors;
-Target toTarget(string s) => s == "X" ? Target.Loss : s == "Y" ? Target.Draw : Target.Win;
+    return (Play) norm;
+}
 
-int roundScore((Play, Play) pair) => playScore(pair.Item2) + resultScore(pair);
-int playScore(Play s) => s == Play.Rock ? 1 : s == Play.Paper ? 2 : 3;
-int resultScore((Play, Play) pair) => pair.Item1 == pair.Item2 ? 3 : wins!.Contains(pair) ? 0 : 6;
+int Score(Play play1, Play play2) =>
+    (int) play2 + 1 + (int) GetResult(play1, play2);
 
-var sum = File.ReadAllText("data.txt").Split(Environment.NewLine)
-    .Select(x => (x.First().ToString(), x.Last().ToString()))
-    .Select(x => (toPlay(x.Item1), toTarget(x.Item2)))
-    .Select(x => (x.Item1, targetToPlay(x)))
-    .Select(x => roundScore(x))
+Result GetResult(Play play1, Play play2) => play1 == play2 ?
+    Result.Draw :
+    PlayForResult(play1, Result.Win) == play2 ?
+        Result.Win :
+        Result.Loss;
+
+var sum = File.ReadAllLines("data.txt")
+    .Select(x => (play: ToPlay(x.First()), result: ToResult(x.Last())))
+    .Select(x => (play1: x.play, play2: PlayForResult(x.play, x.result)))
+    .Select(x => Score(x.play1, x.play2))
     .Sum();
 
 Console.WriteLine(sum);
 
-enum Play { Rock, Paper, Scissors }
-enum Target { Loss, Draw, Win }
+enum Play { Rock = 0, Paper = 1, Scissors = 2 }
+enum Result { Loss = 0, Draw = 3, Win = 6 }
+
+//10835
