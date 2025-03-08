54f08167547a6dc3402b66a02b26b381bb99b5b6
Ian <ian.warburton@lbresearch.com>
2025-03-08 04:02:02 +0000

    Day 20 - Part 2

--- Day20/Program.cs
+++ Day20/Program.cs
@@ -1,120 +1,108 @@
-﻿var numbers = System.IO.File.ReadAllLines("data.txt")
+﻿using System.Numerics;
+
+var numbers = System.IO.File.ReadAllLines("data.txt")
 	.Select(x => int.Parse(x))
 	.Select(x => new Number(x))
 	.ToList();
 
 var numbersList = new LinkedList<Number>(numbers);
-var count = 1;
+var count = 0;
+var length = numbersList.Count;
 
-foreach (var num in numbers)
+void Mix()
 {
-	var listItem = numbersList.Nodes().First(n => n.Value.Id == num.Id);
+	count++;
 
-	var items = numbersList.Nodes().Where(n => n.Value.Id == num.Id).ToList();
-	if (items.Count() > 1)
+	foreach (var num in numbers)
 	{
-			var r = 4;
-	}
+		var listItem = numbersList.Nodes().Single(n => n.Value.Id == num.Id);
 
-	if (listItem.Value.Num == 0)
-		continue;
+		if (listItem.Value.Num == 0)
+			continue;
 
-	var nextItem = listItem;
+		var nextItem = listItem;
 
-	var i = 1;
-	for (;i <= Math.Abs(listItem.Value.Num); i++)
-	{
+		var i = 1;
+		int moves = (int)(listItem.Value.Num % (length - 1));
+		for (;i <= Math.Abs(moves); i++)
+		{
+			if (listItem.Value.Num > 0)
+			{
+				nextItem = nextItem.Next;
+				nextItem ??= numbersList.First;
+			}
+			else
+			{
+				nextItem = nextItem.Previous;
+				nextItem ??= numbersList.Last;
+			}
+			if (i == 1)
+			{
+				numbersList.Remove(listItem);
+			}
+		}
 
 		if (listItem.Value.Num > 0)
 		{
-			nextItem = nextItem.Next;
-			if (nextItem == null)
-				nextItem = numbersList.First;
+			numbersList.AddAfter(nextItem, listItem);
 		}
-		else
+		else 
 		{
-			nextItem = nextItem.Previous;
-			if (nextItem == null)
-				nextItem = numbersList.Last;
+			numbersList.AddBefore(nextItem, listItem);
 		}
 	}
+}
 
-	
-
-	if (listItem.Value.Num < 0)
-	{
-			nextItem = nextItem.Previous;
-			if (nextItem == null)
-				nextItem = numbersList.Last;
-	}
-	else if (nextItem.Next == null)
-	{
-		nextItem = numbersList.First;
-		numbersList.AddBefore(nextItem, listItem);
-		continue;
-	}
-	
-numbersList.Remove(listItem);
-	numbersList.AddAfter(nextItem, listItem);
-
-	count++;
-	/*Console.WriteLine($"Run: {count}");
+for (var i = 0; i < 10; i++)
+{
+	/*Console.WriteLine($"Run: {count} ");
 	foreach (var num0 in numbersList)
-		Console.WriteLine(num0);
-		Console.WriteLine($"{listItem.Value.Num} {i}: {nextItem.Value}");*/
+		Console.Write($"{num0.Num} ");
+
+	Console.WriteLine("");*/
+
+	Mix();
 }
 
 Console.WriteLine($"Run: {count}");
 
-var zeroItem = numbersList.Nodes().First(n => n.Value.Num == 0);
+var zeroItem = numbersList.Nodes().Single(n => n.Value.Num == 0);
 var index1 = GetValue(1000);
 var index2 = GetValue(2000);
 var index3 = GetValue(3000);
 
-Console.WriteLine($"3000: {index1}");
+Console.WriteLine($"1000: {index1}");
 Console.WriteLine($"2000: {index2}");
 Console.WriteLine($"3000: {index3}");
 Console.WriteLine($"Co-ordinates: {index1 + index2 + index3}");
 
-int GetValue(int index)
+BigInteger GetValue(int index)
 {
 	var item = zeroItem;
 	for (var i = 1; i <= index; i++)
 	{
 		item = item.Next;
-		if (item == null)
-			item = numbersList.First;
+		item ??= numbersList.First!;
 	}
 	return item.Value.Num;
 }
-
-
-
-
-/*foreach (var num in numbersList)
-	Console.WriteLine(num);*/
 
 public static class LinkedListExtensions
 {
 	public static IEnumerable<LinkedListNode<T>> Nodes<T>(this LinkedList<T> list)
 	{
-			for (var node = list.First; node != null; node = node.Next)
-			{
-					yield return node;
-			}
+		for (var node = list.First; node != null; node = node.Next)
+		{
+			yield return node;
+		}
 	}
 }
 
-
 class Number
 {
-	public Number(int num)
-	{
-		Num = num;
-		Id = Guid.NewGuid();
-	}
+    public Number(int num) => Num = num * (BigInteger) 811589153;
 
-	public Guid Id { get; }	
+    public Guid Id { get; } = Guid.NewGuid();
 
-	public int Num { get; }	
+    public BigInteger Num { get; }
 };
